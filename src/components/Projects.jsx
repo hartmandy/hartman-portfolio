bbf501ed00526443b098c38235e8db9c39873a1d
hartmandy <120344775+hartmandy@users.noreply.github.com>
2024-01-09 22:39:02 -0500

    updated portfolio cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import image1 from "../images/bmc.png";
 import image2 from "../images/edu.png";
-import image3 from "../images/visualarchives.png";
+import image3 from "../images/jenzabarchatbot.png";
+import image4 from "../images/roofing.png";
+import image5 from "../images/avl.png";
+import image6 from "../images/visualarchives.png";
 
 function Projects() {
   return (
@@ -21,13 +24,34 @@ function Projects() {
         />
       </a>
 
+      <a href="https://jenzabar.com/product/chatbot">
+        <img src={image3} alt="Jenzabar" className=" rounded-lg mr-6 sm:mx-3" />
+      </a>
+
+      <a href="https://www.linkedin.com/in/hartmandy/">
+        <img
+          src={image4}
+          alt="Roofing Map"
+          className=" rounded-lg mr-6 sm:mx-3"
+        />
+      </a>
+
+      <a href="https://collection.ashevilleart.org/portfolios/list?records=18">
+        <img
+          src={image5}
+          alt="Asheville Art Museum"
+          className=" rounded-lg mr-6 sm:mx-3"
+        />
+      </a>
+
       <a href="https://www.visualarchives.org">
         <img
-          src={image3}
+          src={image6}
           alt="Visual Archives"
           className=" rounded-lg mr-6 sm:mx-3"
         />
       </a>
+
       <a href="https://www.linkedin.com/in/hartmandy/" className="p-3">
         View More
       </a>
